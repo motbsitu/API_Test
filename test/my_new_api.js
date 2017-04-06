cc089413ac9386b8b8ec7e20f3587c1e45fe747a
Julie S Mike <juliesmike@gmail.com>
2017-04-06 14:36:50 -0500

    test fixed and passed

--- test/my_new_api.js
+++ test/my_new_api.js
@@ -1,7 +1,7 @@
-var assert = require(“chai”).assert;
-var myNewApi = require(“../lib/my_new_api.js”);
-describe(“myNewApi”, function () {
-  it(“exports handleRequest”, function ()
-    { assert.typeOf(myNewApi.handleRequest, “function”);
+var assert = require("chai").assert;
+var myNewApi = require("../lib/my_new_api.js");
+describe("myNewApi", function () {
+  it("exports handleRequest", function ()
+    { assert.typeOf(myNewApi.handleRequest, "function");
     });
 });
